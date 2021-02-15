9476ec9dea7ab31c417f2862c536005fec0320ee
J-Rozas <jrozaslima@gmail.com>
2021-02-15 01:38:56 +0000

    Update tests.

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -4,5 +4,6 @@ class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal full_title, 'Sample App'
     assert_equal full_title("Help"), 'Help | Sample App'
+    assert_equal full_title('Sign up'), 'Sign up | Sample App'
   end
 end
