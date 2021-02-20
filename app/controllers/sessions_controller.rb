57f6e1a96972714600879d73ef6f13f65b890f74
J-Rozas <jrozaslima@gmail.com>
2021-02-20 21:22:23 +0000

    Add code to handle a valid log in attempt.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log in user and render user's page
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
