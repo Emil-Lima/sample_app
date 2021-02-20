27ee248f39fc6bae21c2cbc7cffce5dfffefee9f
J-Rozas <jrozaslima@gmail.com>
2021-02-20 19:45:38 +0000

    Add action 'create' and action 'destroy'.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:sesion][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log in user and render user's page
+    else
+      # Create an error message
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
