f56ce1afa92ed21f9db953d96bec908274525481
J-Rozas <jrozaslima@gmail.com>
2021-02-21 02:54:36 +0000

    Add test for valid log in.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:example)
+  end
+
   test 'invalid log in' do
     get login_path
     assert_template 'new'
@@ -11,4 +15,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test 'valid log in' do
+    get login_path
+    assert_template 'new'
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path, count: 1
+    assert_select 'a[href=?]', user_path(@user), count: 1
+  end 
 end
