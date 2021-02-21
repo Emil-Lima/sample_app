10c45e1e9db522246d8b5b9c95214413437ac805
J-Rozas <jrozaslima@gmail.com>
2021-02-21 01:32:12 +0000

    Fix bug that prevents users to be logged in.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,8 @@ module SessionsHelper
   end
 
   def current_user
-    @user ||= User.find_by(id: session{ :user_id })
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 end
