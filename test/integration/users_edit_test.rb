2b975bd10d6f0e5326adb9c1c400031af0f6ecb4
J-Rozas <jrozaslima@gmail.com>
2021-02-26 14:59:54 +0000

    Create new test for valid edit requests

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -16,4 +16,17 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     assert_select 'div', 'The form contains 3 errors'
   end
+
+  test 'valid edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "New Name"
+    email = "thisvalid@email.com"
+    patch user_path(@user), params: { user: { name: name, email: email, password: "", password_confirmation: "" } }
+    refute flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
