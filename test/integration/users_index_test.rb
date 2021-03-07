b0601c194735ce4d2098515749157899736c743f
J-Rozas <jrozaslima@gmail.com>
2021-03-07 02:56:25 +0000

    Update test for pagination with active users

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -9,14 +9,16 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.first.toggle!(:activated)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
-    first_page_of_users.each do |user|
+    assigns(:users).each do |user|
+      assert user.activated?
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
-        assert_select 'a[href=?]', user_path(user), test: 'delete'
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
     assert_difference 'User.count', -1 do
