51ae5e258df91021fe7c94a7cf59e90e4bb53c1c
J-Rozas <jrozaslima@gmail.com>
2021-03-11 14:35:37 +0000

    Update actions to handle a proper password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update] # Checks for an expired password reset
 
   def new
   end
@@ -20,8 +21,29 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+  
+  def update
+    if params[:user][:password].empty?                  # Checks for empty password
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update(user_params)                     # Valid attempt
+      @user.forget
+      reset_session
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'                                     # Failed due to invalid password
+    end
+  end
 
   private
+
+    # Before filters
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
 
     def get_user
       @user = User.find_by(email: params[:email])
@@ -31,6 +53,14 @@ class PasswordResetsController < ApplicationController
     def valid_user
       unless (@user && @user.activated && @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
+      end
+    end
+
+    # Checks expiration of reset token.
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
       end
     end
 end
